edc46de4cca94f648244ab99fe96810e6837e930
mac-n <39732428+mac-n@users.noreply.github.com>
2021-04-01 22:59:10 +0100

    Update step 0 recode age and CDRSB.R

--- step 0 recode age and CDRSB.R
+++ step 0 recode age and CDRSB.R
@@ -1,32 +1,34 @@
+#this script recodes age and CDRSB variables from the dataframe
+
 require(ADNIMERGE)
 data(adnimerge)
 
 bl_adni<-adnimerge
-
+#adjust age variable from baseline
 rnum <- nrow(bl_adni) 
-# for(i in 1:rnum){
-#   if (bl_adni$VISCODE[[i]] == "m06") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 0.5
-#   if (bl_adni$VISCODE[[i]] == "m12") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 1
-#   if (bl_adni$VISCODE[[i]] == "m18") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 1.5
-#   if (bl_adni$VISCODE[[i]] == "m24") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 2
-#   if (bl_adni$VISCODE[[i]] == "m30") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 2.5
-#   if (bl_adni$VISCODE[[i]] == "m36") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 3
-#   if (bl_adni$VISCODE[[i]] == "m42") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 3.5
-#   if (bl_adni$VISCODE[[i]] == "m48") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 4
-#   if (bl_adni$VISCODE[[i]] == "m54") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 4.5
-#   if (bl_adni$VISCODE[[i]] == "m60") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 5
-#   if (bl_adni$VISCODE[[i]] == "m66") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 5.5
-#   if (bl_adni$VISCODE[[i]] == "m72") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 6
-#   if (bl_adni$VISCODE[[i]] == "m78") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 6.5
-#   if (bl_adni$VISCODE[[i]] == "m84") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 7
-#   if (bl_adni$VISCODE[[i]] == "m90") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 7.5
-#   if (bl_adni$VISCODE[[i]] == "m96") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 8
-#   if (bl_adni$VISCODE[[i]] == "m102") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 8.5
-#   if (bl_adni$VISCODE[[i]] == "m108") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 9
-#   if (bl_adni$VISCODE[[i]] == "m114") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 9.5
-#   if (bl_adni$VISCODE[[i]] == "m120") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 10
-#   end 
-# }
+ for(i in 1:rnum){
+   if (bl_adni$VISCODE[[i]] == "m06") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 0.5
+   if (bl_adni$VISCODE[[i]] == "m12") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 1
+   if (bl_adni$VISCODE[[i]] == "m18") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 1.5
+   if (bl_adni$VISCODE[[i]] == "m24") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 2
+   if (bl_adni$VISCODE[[i]] == "m30") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 2.5
+   if (bl_adni$VISCODE[[i]] == "m36") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 3
+   if (bl_adni$VISCODE[[i]] == "m42") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 3.5
+   if (bl_adni$VISCODE[[i]] == "m48") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 4
+   if (bl_adni$VISCODE[[i]] == "m54") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 4.5
+   if (bl_adni$VISCODE[[i]] == "m60") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 5
+   if (bl_adni$VISCODE[[i]] == "m66") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 5.5
+   if (bl_adni$VISCODE[[i]] == "m72") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 6
+   if (bl_adni$VISCODE[[i]] == "m78") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 6.5
+   if (bl_adni$VISCODE[[i]] == "m84") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 7
+   if (bl_adni$VISCODE[[i]] == "m90") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 7.5
+   if (bl_adni$VISCODE[[i]] == "m96") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 8
+   if (bl_adni$VISCODE[[i]] == "m102") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 8.5
+   if (bl_adni$VISCODE[[i]] == "m108") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 9
+   if (bl_adni$VISCODE[[i]] == "m114") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 9.5
+   if (bl_adni$VISCODE[[i]] == "m120") bl_adni$AGE[[i]] <- bl_adni$AGE[[i]] + 10
+   end 
+ }
 
 
 df<-bl_adni
